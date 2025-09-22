958ed8cfb971a0286078e8dc3130752daa250f56
SamLee <goha2010@naver.com>
2025-09-23 07:37:06 +0900

    chore: docs 출력으로 변경(outDir=docs)

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/Wedding-invitation/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
